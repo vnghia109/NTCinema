9ac7f607537061e71ee04810c5d41b4ccb1e93ad
vnghia109 <vnghiahi@gmail.com>
2024-03-27 23:00:43 +0700

    fix entity showTime, schedule, room; related to showTime, room, movie, food...

--- src/main/java/vn/iostar/NT_cinema/controller/FoodController.java
+++ src/main/java/vn/iostar/NT_cinema/controller/FoodController.java
@@ -1,6 +1,7 @@
 package vn.iostar.NT_cinema.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.iostar.NT_cinema.dto.GenericResponse;
@@ -12,8 +13,10 @@ public class FoodController {
     @Autowired
     FoodService foodService;
     @GetMapping("")
-    public ResponseEntity<GenericResponse> getFoods(@RequestParam(defaultValue = "") String type){
-        return foodService.getFoods(type);
+    public ResponseEntity<GenericResponse> getFoods(@RequestParam(defaultValue = "") String type,
+                                                    @RequestParam(defaultValue = "1") int index,
+                                                    @RequestParam(defaultValue = "10") int size){
+        return foodService.getFoods(type, PageRequest.of(index-1, size));
     }
 
     @GetMapping("/{id}")
--- src/main/java/vn/iostar/NT_cinema/repository/FoodRepository.java
+++ src/main/java/vn/iostar/NT_cinema/repository/FoodRepository.java
@@ -1,5 +1,8 @@
 package vn.iostar.NT_cinema.repository;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import vn.iostar.NT_cinema.constant.FoodType;
@@ -9,5 +12,9 @@ import java.util.List;
 
 @Repository
 public interface FoodRepository extends MongoRepository<Food, String> {
-    List<Food> findAllByFoodType(FoodType foodType);
+    Page<Food> findAllByFoodType(FoodType foodType, Pageable pageable);
+
+    @Override
+    @NotNull
+    Page<Food> findAll(@NotNull Pageable pageable);
 }
