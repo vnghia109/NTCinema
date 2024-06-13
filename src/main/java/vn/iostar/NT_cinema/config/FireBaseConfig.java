8a78b2906764225fbed3420c5adbb0adb9f9adfc
vnghia109 <vnghiahi@gmail.com>
2024-06-14 00:48:10 +0700

    fix FireBaseConfig

--- src/main/java/vn/iostar/NT_cinema/config/FireBaseConfig.java
+++ src/main/java/vn/iostar/NT_cinema/config/FireBaseConfig.java
@@ -3,17 +3,21 @@ package vn.iostar.NT_cinema.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 
 @Configuration
 public class FireBaseConfig {
+    @Value("${FIREBASE_CREDENTIALS}")
+    private String firebaseConfig;
     @Bean
     FirebaseApp firebaseApp() throws Exception {
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(new FileInputStream("src/main/resources/nt-cinema-firebase-adminsdk-qqxnf-bbd0a156f7.json")))
+                .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(firebaseConfig.getBytes())))
                 .build();
 
         return FirebaseApp.initializeApp(options);
